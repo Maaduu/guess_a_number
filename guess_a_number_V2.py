e949b2fe4dd753c79ecb6f6556a288d6ccc28c3a
Maaduu <mamadubalde644@yahoo.com>
2019-05-28 06:03:33 +0200

    Add input range of number to guess & number of trials

--- guess_a_number_V2.py
+++ guess_a_number_V2.py
@@ -1,9 +1,11 @@
-# import random
-# import random as r
+
 from random import randint
-number_to_guess = randint(1, 25)
-# print(number_to_guess)
-number_try = range(5)
+amplitude = input('Enter the range of the number to guess: ')
+amplitude = int(amplitude)
+number_try_input = input('Enter the number of trials: ')
+number_try_input = int(number_try_input)
+number_to_guess = randint(1, amplitude)
+number_try = range(number_try_input)
 for i in number_try:
     attempt = input('Enter a number ({0} attempt): '.format(i + 1))
     attempt = int(attempt)
