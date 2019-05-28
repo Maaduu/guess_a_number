73c8d98fbd31620cd1f28e2b194f9e432a46824d
Maaduu <mamadubalde644@yahoo.com>
2019-05-28 06:06:30 +0200

    Add while

--- guess_a_number_V3.py
+++ guess_a_number_V3.py
@@ -1,12 +1,11 @@
 
 from random import randint
-amplitude = input('Enter the range of the number to guess: ')
-amplitude = int(amplitude)
-number_try_input = input('Enter the number of trials: ')
-number_try_input = int(number_try_input)
-number_to_guess = randint(1, amplitude)
-number_try = range(number_try_input)
-for i in number_try:
+number_to_guess = randint(1, 25)
+number_try = 5
+
+i = 0
+i = int(i)
+while i < number_try:
     attempt = input('Enter a number ({0} attempt): '.format(i + 1))
     attempt = int(attempt)
 
@@ -18,6 +17,7 @@ for i in number_try:
         print('Bravooo! You have won in {0} attempt(s)'.format(
             i + 1))
         break
+    i += 1
 if attempt != number_to_guess:
     print('You lost.')
     print('The number to guess was: {0}'.format(number_to_guess))
